90601fad4c412dd9607d682130ddf22220256545
KwonJJ <ejtkejqkf@naver.com>
2023-07-01 23:05:25 +0900

    bin commit

--- Bak_Algorithm/src/Basic/Main.java
+++ Bak_Algorithm/src/Basic/Main.java
@@ -1,15 +1,8 @@
 package Basic;
 
-
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		
-		System.out.println("수 입력 : ");
-		
 	}
 	
 }
