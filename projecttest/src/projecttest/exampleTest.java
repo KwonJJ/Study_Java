fafffcbbb67ac006ea26fc317e2c71f6b5d93174
KwonJJ <ejtkejqkf@naver.com>
2023-07-01 17:24:15 +0900

    test commit

---
+++ projecttest/src/projecttest/exampleTest.java
@@ -0,0 +1,16 @@
+package projecttest;
+
+public class exampleTest {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("test");
+		System.out.println("테스트");
+		
+		/* 
+		 * 깃 주석 테스트
+		 * 깃 연동 test
+		 */
+	}
+
+}
