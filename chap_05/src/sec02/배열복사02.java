5f6a63825d4e8c7ba6504e01526adb75d5ad8ecd
KwonJJ <136597007+KwonJJ@users.noreply.github.com>
2023-07-03 13:31:08 +0900

    system.arraycopy_Add

--- chap_05/src/sec02/배열복사02.java
+++ chap_05/src/sec02/배열복사02.java
@@ -10,6 +10,13 @@ public class 배열복사02 {
 		//oldIntArray 복사 -> 0부터 시작
 		//newIntArray 복사 -> 0부터 시작
 		//마지막 oldIntArray.length는 복사할 개수
+		/*System.arraycopy(src, srcPos, dest, destPos, length);
+		 * src = 원본 배열
+		 * srcPos = 원본 배열에서 복사할 항목의 시작 인덱스
+		 * dest = 새 배열
+		 * destPos = 새 배열에서 붙여넣을 시작 인덱스
+		 * 새 배열 변수.length = 복사할 개수
+		 */
 		
 		for(int i = 0; i < oldIntArray.length; i++) { //수동으로 배열 복사
 			newIntArray[i] = oldIntArray[i];
